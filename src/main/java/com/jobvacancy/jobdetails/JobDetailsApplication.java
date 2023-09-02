ffba502365b8819b7762360a22469fc02ce71deb
Fazar Rahman <fazar.rahman@gmail.com>
2023-09-02 13:22:23 +0700

    Add spring boot project

---
+++ src/main/java/com/jobvacancy/jobdetails/JobDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.jobvacancy.jobdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobDetailsApplication.class, args);
+	}
+
+}
